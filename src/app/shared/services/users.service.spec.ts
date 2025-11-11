a3105eab9990a5f75d653beedb33dd80d63dba44
qetiproject <ketideveloper@gmail.com>
2025-11-11 20:40:25 +0400

    service

---
+++ src/app/shared/services/users.service.spec.ts
@@ -0,0 +1,36 @@
+import { TestBed } from '@angular/core/testing';
+import { UserInteface } from '../types/user.interface';
+import { UsersService } from "./users.service";
+
+describe('UsersService', () => {
+  let usersService: UsersService
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [UsersService]
+    });
+
+    usersService = TestBed.inject(UsersService);
+  });
+  it('creates a service', () => {
+    expect(usersService).toBeTruthy();
+  });
+
+  describe('addUser', () => {
+    it('should add a user', () => {
+      const user: UserInteface = {
+        id: '3',
+        name: 'foo'
+      }
+      usersService.addUser(user);
+      expect(usersService.users).toEqual([{id: '3', name: 'foo'}]);
+    })
+  });
+
+  describe('removeUser', () => {
+    it('should remove a user', () => {
+      usersService.users = [{ id: '3', name: 'foo'}]
+      usersService.removeUser('3');
+      expect(usersService.users).toEqual([])
+    })
+  })
+})
