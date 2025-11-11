14ad11043875fc76258b997c79c41d7aa4854113
qetiproject <ketideveloper@gmail.com>
2025-11-11 19:00:11 +0400

    testing utils

---
+++ src/app/shared/utils/utils.spec.ts
@@ -0,0 +1,20 @@
+import { pluck, range } from './utils';
+
+describe('utils', () => {
+  describe('range', () => {
+    it('returns correct range from 1 to 5', () => {
+      expect(range(1, 5)).toEqual([1, 2, 3, 4]);
+    });
+  });
+
+  describe('pluck', () => {
+    it('returns correct result', () => {
+      const data  = [
+        {id: '1', name: 'foo'},
+        {id: '2', name: 'bar'},
+        {id: '3', name: 'baz'}
+      ]
+      expect(pluck(data, 'id')).toEqual(['1', '2', '3']);
+    });
+  })
+})
