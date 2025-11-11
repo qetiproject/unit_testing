59a37f842ed3e0a2172109d6f3e2d7ca30df2bb6
qetiproject <ketideveloper@gmail.com>
2025-11-11 21:15:55 +0400

    spyon

--- src/app/shared/services/users.service.spec.ts
+++ src/app/shared/services/users.service.spec.ts
@@ -1,16 +1,28 @@
 import { TestBed } from '@angular/core/testing';
 import { UserInteface } from '../types/user.interface';
-import { UsersService } from "./users.service";
+import { UsersService } from './users.service';
+import { UtilsService } from './utils.service';
 
 describe('UsersService', () => {
-  let usersService: UsersService
+  let usersService: UsersService;
+  let utilsService: UtilsService;
+  // const utilsServiceMock = {
+  //   pluck: jest.fn(),
+  // };
+
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [UsersService]
+      providers: [
+        UsersService,
+        UtilsService,
+        // { provide: UtilsService, useValue: utilsServiceMock },
+      ],
     });
 
     usersService = TestBed.inject(UsersService);
+    utilsService = TestBed.inject(UtilsService);
   });
+
   it('creates a service', () => {
     expect(usersService).toBeTruthy();
   });
@@ -19,18 +31,32 @@ describe('UsersService', () => {
     it('should add a user', () => {
       const user: UserInteface = {
         id: '3',
-        name: 'foo'
-      }
+        name: 'foo',
+      };
       usersService.addUser(user);
-      expect(usersService.users).toEqual([{id: '3', name: 'foo'}]);
-    })
+      expect(usersService.users).toEqual([{ id: '3', name: 'foo' }]);
+    });
   });
 
   describe('removeUser', () => {
     it('should remove a user', () => {
-      usersService.users = [{ id: '3', name: 'foo'}]
+      usersService.users = [{ id: '3', name: 'foo' }];
       usersService.removeUser('3');
-      expect(usersService.users).toEqual([])
-    })
-  })
-})
+      expect(usersService.users).toEqual([]);
+    });
+  });
+
+  describe('getUsernames', () => {
+    it('should get usernames', () => {
+      jest.spyOn(utilsService, 'pluck');
+      usersService.users = [{ id: '3', name: 'foo' }];
+      usersService.getUsernames();
+      expect(utilsService.pluck).toHaveBeenCalledWith(
+        usersService.users,
+        'name'
+      );
+      // utilsServiceMock.pluck.mockReturnValue(['foo']);
+      // expect(usersService.getUsernames()).toEqual(['foo']);
+    });
+  });
+});
