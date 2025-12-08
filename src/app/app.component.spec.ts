03774e6374d3dff2d8d7cc16d4bd08e8582c6072
qetiproject <ketideveloper@gmail.com>
2025-12-08 14:05:58 +0400

    todo service testing

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,19 +1,19 @@
-import { ComponentFixture, TestBed } from "@angular/core/testing";
-import { AppComponent } from "./app.component";
+// import { ComponentFixture, TestBed } from "@angular/core/testing";
+// import { AppComponent } from "./app.component";
 
-describe('AppComponent', () => {
-  let fixture: ComponentFixture<AppComponent>;
-  let component: AppComponent;
+// describe('AppComponent', () => {
+//   let fixture: ComponentFixture<AppComponent>;
+//   let component: AppComponent;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({ declarations: [AppComponent] });
-    fixture = TestBed.createComponent(AppComponent);
-    component = fixture.componentInstance;
-  });
+//   beforeEach(() => {
+//     TestBed.configureTestingModule({ declarations: [AppComponent] });
+//     fixture = TestBed.createComponent(AppComponent);
+//     component = fixture.componentInstance;
+//   });
 
-  it('should increment count when button clicked', () => {
-    const button = fixture.nativeElement.querySelector('button');
-    button.click();
-    expect(component.count).toBe(1);
-  });
-});
+//   it('should increment count when button clicked', () => {
+//     const button = fixture.nativeElement.querySelector('button');
+//     button.click();
+//     expect(component.count).toBe(1);
+//   });
+// });
